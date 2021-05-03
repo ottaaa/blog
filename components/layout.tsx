f5c4f1857838ba920d4c8c3c094a284322e0fc40
太田勝博 <styxcab@gmail.com>
2021-05-04 02:52:26 +0900

    一旦画像の最適化を削除

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
@@ -34,8 +33,7 @@ export default function Layout({
             <header className={styles.header}>
                 {home ? (
                     <>
-                        <Image
-                            priority
+                        <img
                             src="/images/profile.jpg"
                             className={utilStyles.borderCircle}
                             height={144}
@@ -48,8 +46,7 @@ export default function Layout({
                     <>
                         <Link href="/">
                             <a>
-                                <Image
-                                    priority
+                                <img
                                     src="/images/profile.jpg"
                                     className={utilStyles.borderCircle}
                                     height={108}
