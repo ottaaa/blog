66f49563742e80895df097843d338aa640402a29
太田勝博 <styxcab@gmail.com>
2021-05-04 06:02:56 +0900

    test

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const { PHASE_PRODUCTION_BUILD } = require('next/constants');
-
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === PHASE_PRODUCTION_BUILD) {
-        return { assetPrefix: 'blog' };
-    }
-    return {};
-};
